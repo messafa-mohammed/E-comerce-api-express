cda123137413737c7d9ab54ef619643ab03500ca
Piuma Nera <moh.messafa@gmail.com>
2024-03-13 18:07:56 +0100

    create user

---
+++ controllers/userController.js
@@ -0,0 +1,67 @@
+const {validationResult } = require( "express-validator");
+const bcrypt = require('bcrypt');
+const randomstring = require('randomstring')
+const User = require( "../models/userModel");
+
+const createUser = async(req,res)=>{
+    try{
+        const errors = validationResult(req)
+       
+        if(!errors){
+            return res.status(200).json({
+                success: false,
+                msg:'Errors',
+                errors:errors.array()
+            });
+        }
+         
+        const {name, email} = req.body;
+        
+        const isExists = await  User.findOne({email});
+        
+        if (isExists) {
+            return res.status(409).json({
+                success:false,
+                msg:"Email already exists"
+            })
+        }
+        
+        const password = randomstring.generate(6);
+        const hashPassword = await bcrypt.hash(password,10); 
+       
+        var obj = {
+            name,
+            email,
+            password : hashPassword
+        };
+
+        if(req.body.role && req.body.role == 1){
+            return res.status(400).json({
+                success:false,
+                msg:"You don't have permission to add admin user."
+            })
+        }else if(req.body.role){
+            obj.role=req.body.role;
+        }
+
+        const user = new User(obj);
+        const userData = await user.save();
+        
+        return res.status(200).json({
+            success:true,
+            msg:"user created successfully",
+            data:userData
+        })
+
+
+    }catch(error){
+        return res.status(400).json({
+            reccess:false,
+            msg: error.message
+        });
+    }
+}
+
+module.exports = {
+    createUser,
+}
--- routes/commonRoute.js
+++ routes/commonRoute.js
@@ -4,6 +4,9 @@ const auth = require('../middlewares/authMiddleware')
 const { categoryAddValidator , categoryDeleteValidator , categoryUpdateValidator , postCreateValidator , postUpdateValidator , postDeleteValidator} = require('../helpers/adminValidator')
 const categoryController = require('../controllers/categoryController')
 const postController = require('../controllers/postController')
+const {createUserValidator} = require('../helpers/validator')
+const userController = require('../controllers/userController')
+ 
 
 // category routes
 router.post('/add-category', auth , categoryAddValidator , categoryController.addCategory )
@@ -16,6 +19,9 @@ router.post('/create-post', auth , postCreateValidator , postController.createPo
 router.get('/get-posts', auth , postController.getPosts)
 router.post('/delete-post', auth , postDeleteValidator , postController.deletePost)
 router.post('/update-post', auth , postUpdateValidator , postController.updatePost)
+
+//user routes
+router.post('/create-user',auth , createUserValidator , userController.createUser)
 
 
 module.exports = router;
