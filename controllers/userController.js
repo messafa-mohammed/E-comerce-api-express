d271380af35441d94d5783c6a3178e4b528103f3
Piuma Nera <moh.messafa@gmail.com>
2024-03-14 13:45:45 +0100

    send email done

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ const {validationResult } = require( "express-validator");
 const bcrypt = require('bcrypt');
 const randomstring = require('randomstring')
 const User = require( "../models/userModel");
+const {sendIt} = require('../helpers/mailer')
 
 const createUser = async(req,res)=>{
     try{
@@ -48,7 +49,17 @@ const createUser = async(req,res)=>{
         const userData = await user.save();
         console.log(password);
 
-        
+
+        const content = `
+                        <p>Hey <b>`+userData.name+`,</b> Your account is created, below is your details. </p>
+                        <table style="border-style:none;">
+                            <tr><th>Name: </th><td>`+userData.name+`</td></tr>
+                            <tr><th>Email: </th><td>`+userData.email+`</td></tr>
+                            <tr><th>Password: </th><td>`+password+`</td></tr>
+                        </table>
+                        <p>Now you can login your account in Our Application ,Thanks....</p>
+        `;
+        sendIt(userData.email, 'Account Created', content )
         
         return res.status(200).json({
             success:true,
--- helpers/mailer.js
+++ helpers/mailer.js
@@ -1,43 +1,40 @@
-const nodemailer = require('nodemailer');
 
-const transporter = nodemailer.createTransport({
-                            host: process.env.SMTP_HOST,
-                            port: process.env.SMTP_PORT,
-                            secure:false,
-                            requireTLS:true,
-                            auth:{
-                                user: process.env.SMTP_MAIL,
-                                pass: process.env.SMTP_PASSWORD
-                            }
-})
 
-const sendMail = async(email , subject , content) => {
+var nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.SMTP_MAIL,
+    pass: process.env.SMTP_PASSWORD
+  }
+});
+
+const sendIt = async(email , subject , content) =>{
     try{
 
         var mailOptions = {
-            from: process.env.SMTP_MAIL,
-            to: email,
-            subject: subject,
-            html: content,
+        from: process.env.SMTP_MAIL,
+         to: email,
+         subject: subject,
+        html: content
         };
 
-        transporter.sendMail(mailOptions , (error , info) => {
-
-            if(error){
-                console.log(error);
-            }
-            console.log(`Email sent : ${info.response}`, info.messageId);  
-            
-
-
+        transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(`onh : ${error}`);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
         });
-
 
     }catch(error){
         console.log("Error : ", error.message);
     }
 }
 
+
+
 module.exports = {
-    sendMail
+    sendIt
 }
